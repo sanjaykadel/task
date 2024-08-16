447d83cf6f0412cd334cebec1ef86fff84fc8c75
sanjay <kadelsanjay05@gmail.com>
2024-08-16 15:21:11 +0545

    question no 5

--- mutate_string.py
+++ mutate_string.py
@@ -12,10 +12,8 @@ def mutate_string(string, position, character):
 
 # Input lina
 input_string = input("Enter the string: ")  # User bata string input lina
-
-# Position ra character alag-alag input lina
-position = int(input("Enter the position (index) start with 0 as 1 : "))  # Position lai integer ma convert garne
-character = input("Enter the character to insert: ")  # Character input lina
+position, character = input("Enter the position and character separated by space: ").split()  # Position ra character input lina
+position = int(position)  # Position lai integer ma convert garne
 
 # Function call garne ra result print garne
 result = mutate_string(input_string, position, character)  # Function lai call garne
